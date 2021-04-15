70aedf071381e0c12ffd6188ab61e932494a22d2
Sivannsolum <64545343+Sivannsolum@users.noreply.github.com>
2021-04-15 11:15:34 +0530

    Css changes lbs sidebar

--- frontend/src/components/SidebarMenu/LbsSidebarMenu.vue
+++ frontend/src/components/SidebarMenu/LbsSidebarMenu.vue
@@ -1,69 +1,50 @@
 <template>
-  <v-card
-    class="d-inline-flex pa-2"
-    outlined
-    tile
-  >
-    <div>
-        <p><span class="white--text text-uppercase">View Mode</span></p>
+  <v-card class="d-inline-flex pa-2" outlined tile >
+    <div style="padding:10px">
+      <p >
+        <span
+          class="white--text text-uppercase"
+          style="font-size: x-small; font-weight: bold"
+          >View Mode</span
+        >
+      </p>
+      <v-row>
         <v-checkbox dense label="Zone" class="d-inline-block"></v-checkbox>
         <v-checkbox dense label="Interior" class="d-inline-block"></v-checkbox>
         <v-checkbox dense label="Text" class="d-inline-block"></v-checkbox>
         <v-checkbox dense label="Facility" class="d-inline-block"></v-checkbox>
+      </v-row>
 
-    <div class="text-center">
-    <v-btn
-      class="ma-2"
-      outlined
-      color="indigo"
-    >
-      Management
-    </v-btn>
-    </div>
-    <div class="text-center">
-    <v-btn
-      class="ma-2"
-      outlined
-      color="indigo"
-    >
-      Map Design
-    </v-btn>
-    </div>
-    <div class="text-center">
-    <v-btn
-      class="ma-2"
-      outlined
-      color="indigo"
-    >
-      Self Location Link
-    </v-btn>
-    </div>
-    <div class="text-center">
-    <v-btn
-      class="ma-2"
-      outlined
-      color="indigo"
-    >
-      Stocking &#38; Pricing
-    </v-btn>
-    </div>
-    <div class="text-center">
-    <v-btn
-      class="ma-2"
-      outlined
-      color="indigo"
-    >
-      Setting
-    </v-btn>
-    </div>
+      <div class="text-center">
+        <v-btn class="ma-2" outlined color="white"> Management </v-btn>
+      </div>
+      <div class="text-center">
+        <v-btn class="ma-2" outlined color="white"> Map Design </v-btn>
+      </div>
+      <div class="text-center">
+        <v-btn class="ma-2" outlined color="white"> Self Location Link </v-btn>
+      </div>
+      <div class="text-center">
+        <v-btn class="ma-2" outlined color="white">
+          Stocking &#38; Pricing
+        </v-btn>
+      </div>
+      <div class="text-center">
+        <v-btn class="ma-2" outlined color="white"> Setting </v-btn>
+      </div>
     </div>
   </v-card>
 </template>
 <style>
 .theme--light.v-label {
-    color: rgb(255 255 255);
+  color: rgb(255 255 255) !important;
+  font-size: small;
+  left: -7px;
 }
 .v-btn:not(.v-btn--round).v-size--default {
-    min-width: 199px;
+  min-width: 212px;
+}
+.v-application .pa-2 {
+    padding: 4px !important;
 }
 </style>
