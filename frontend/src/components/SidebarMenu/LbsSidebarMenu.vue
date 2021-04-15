fd6b4af7035ea05d9e0d617ba15756ee810411a4
Animesh <35163246+Animesh033@users.noreply.github.com>
2021-04-15 12:51:27 +0530

    LBS sidebar dropdown

--- frontend/src/components/SidebarMenu/LbsSidebarMenu.vue
+++ frontend/src/components/SidebarMenu/LbsSidebarMenu.vue
@@ -32,6 +32,32 @@
       <div class="text-center">
         <v-btn class="ma-2" outlined color="white"> Setting </v-btn>
       </div>
+      <v-list>
+      <v-list-group>
+        <template v-slot:activator>
+          <v-list-item-title>CAD LAYER</v-list-item-title>
+        </template>
+        <div class="ma-2" rounded>
+          <v-checkbox dense label="Layer 1"></v-checkbox>
+          <v-divider></v-divider>
+          <v-checkbox dense label="Layer 2"></v-checkbox>
+          <v-divider></v-divider>
+          <v-checkbox dense label="Layer 3"></v-checkbox>
+          <v-divider></v-divider>
+          <v-checkbox dense label="Layer 4"></v-checkbox>
+          <v-divider></v-divider>
+      </div>
+      </v-list-group>
+    </v-list>
+          <v-divider></v-divider>
+    <v-list>
+      <v-list-group>
+        <template v-slot:activator>
+          <v-list-item-title>MINI MAP</v-list-item-title>
+        </template>
+        <v-textarea></v-textarea>
+      </v-list-group>
+    </v-list>
     </div>
   </v-card>
 </template>
